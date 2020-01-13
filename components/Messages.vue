b8fb1b7f19d57e833427800b675939804d0dc151
Tatsuo Sato <tatsuosato0908@gmail.com>
2020-01-14 01:15:06 +0900

    チャットメッセージのリストを表示させる大枠のコンポーネントを作成

---
+++ components/Messages.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="chats-container">
+    <message />
+    <message />
+    <message />
+    <message />
+    <message />
+    <message />
+    <message />
+    <message />
+    <message />
+    <message />
+    <message />
+    <message />
+    <message />
+    <message />
+    <message />
+    <message />
+    <message />
+    <message />
+    <message />
+  </div>
+</template>
+
+<script>
+import Message from "~/components/Message.vue";
+export default {
+  components: {
+    Message
+  }
+};
+</script>
+
+<style scoped>
+/* scopedはこのvueファイル内のみスタイルを適用される。
+他のvueファイルには一切影響されない */
+.chats-container {
+  padding: 16px;
+}
+</style>
