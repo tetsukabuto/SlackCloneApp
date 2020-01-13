1910e78f4e8ef1ce0ba9e4ae7d89c2dba755830b
Tatsuo Sato <tatsuosato0908@gmail.com>
2020-01-14 03:14:45 +0900

    チャット入力欄をクリックした際、アラートのポップアップを出す処理追加

--- components/ChatForm.vue
+++ components/ChatForm.vue
@@ -1,9 +1,24 @@
 <template>
   <div class="input-container">
-    <textarea></textarea>
+    <!-- v-onはイベント(HTMLに対して起きる変化)を監視する機能。例えばボタンが押されたとかHTMLが表示されたとか -->
+    <!-- フォーム欄をクリックしたときにmethodsで定義したloginを呼び出しポップアップのアラートを出す -->
+    <!-- <textarea v-on:click="login"></textarea> -->
+
+    <!-- v-onは@と省略できる -->
+    <textarea @click="login"></textarea>
   </div>
 </template>
-
+<script>
+export default {
+  //methodsはmethods内に独自のfunctionを定義してそれを呼び出す
+  methods: {
+    //フォーム欄をクリックしたときポップアップのアラートを呼び出すメソッド
+    login() {
+      window.alert("ログインしろよ");
+    }
+  }
+};
+</script>
 <style scoped>
 .input-container {
   padding: 10px;
