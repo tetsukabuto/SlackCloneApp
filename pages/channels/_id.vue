58f2faed06d9f49437badbbdd56181352747b0db
Tatsuo Sato <tatsuosato0908@gmail.com>
2020-01-16 03:55:36 +0900

    チャットメッセージ表示のコンポーネント作成及びメッセージをfirebaseから呼び出す処理実装

---
+++ pages/channels/_id.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="container">
+    <div class="chats-layout">
+      <Messages />
+    </div>
+    <div class="input-layout">
+      <chat-form />
+    </div>
+  </div>
+</template>
+
+<script>
+// importで使用したいコンポーネントを読み込み
+import Messages from "~/components/Messages.vue";
+import ChatForm from "~/components/ChatForm.vue";
+import { db } from "~/plugins/firebase";
+export default {
+  components: {
+    //読み込んだコンポーネントをこのvueファイルで使えるよう設定
+    Messages,
+    ChatForm
+  },
+  data() {
+    return { messages: [] };
+  },
+  mounted() {
+    //nuxtではthis.$routeを指定することでurlに関する情報を取得できる
+    //ここではチャンネルID(documentのid)をルーティングから取得してchannelIdに代入
+    //this.$route.paramsにパラメータの情報が保持されている
+    //_id.vueと指定いる場合はthis.$routes.params.id でパラメーターを取得できる
+    const channelId = this.$route.params.id;
+
+    //channelsというコレクションに保存されているdocumentにあるmessagesのコレクションを指定して保存
+    db.collection("channels")
+      .doc(channelId)
+      .collection("messages")
+      .get()
+      .then(querySnapshot => {
+        querySnapshot.forEach(doc => {
+          this.messages.push({ id: doc.id, ...doc.data() });
+        });
+      });
+  }
+};
+</script>
+
+<style scoped>
+.container {
+  height: 100%;
+}
+
+.chats-layout {
+  overflow: scroll;
+  height: 90%;
+}
+
+.input-layout {
+  height: 10%;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,38 +1,3 @@
 <template>
-  <div class="container">
-    <div class="chats-layout">
-      <Messages />
-    </div>
-    <div class="input-layout">
-      <chat-form />
-    </div>
-  </div>
+  <div class="container"></div>
 </template>
-
-<script>
-// importで使用したいコンポーネントを読み込み
-import Messages from "~/components/Messages.vue";
-import ChatForm from "~/components/ChatForm.vue";
-export default {
-  components: {
-    //読み込んだコンポーネントをこのvueファイルで使えるよう設定
-    Messages,
-    ChatForm
-  }
-};
-</script>
-
-<style scoped>
-.container {
-  height: 100%;
-}
-
-.chats-layout {
-  overflow: scroll;
-  height: 90%;
-}
-
-.input-layout {
-  height: 10%;
-}
-</style>
