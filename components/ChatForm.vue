b5489a74bc993e514fd9070757806a9f38219751
Tatsuo Sato <tatsuosato0908@gmail.com>
2020-01-19 04:13:59 +0900

    チャット入力欄に入力された値をデータバインディングをしてエンターキー押下後、firebaseに登録

--- components/ChatForm.vue
+++ components/ChatForm.vue
@@ -5,17 +5,72 @@
     <!-- <textarea v-on:click="login"></textarea> -->
 
     <!-- v-onは@と省略できる -->
-    <textarea @click="login"></textarea>
+    <!-- <textarea @click="login"></textarea> -->
+
+    <!-- v-modelはHTMLの値が変わるとJavaScriptのデータの値が変わる仕組みのディレクティブ(双方向データバインディング) -->
+    <!-- v-on:keydown.enterはエンターが押されたらaddMessageを実行 -->
+    <textarea v-model="text" v-on:keydown.enter="addMessage"></textarea>
+
+    <!-- ボタン押下でv-model="text"で取得した値をconsol.logで表示 -->
+    <!-- <button v-on:click="checkTextValue">値を確認!</button> -->
   </div>
 </template>
 <script>
+import { db } from "~/plugins/firebase";
+
 export default {
+  data() {
+    return {
+      // v-model="text"で取得した値の初期値
+      //text: "テスト"
+
+      text: null
+    };
+  },
   //methodsはmethods内に独自のfunctionを定義してそれを呼び出す
   methods: {
     //フォーム欄をクリックしたときポップアップのアラートを呼び出すメソッド
-    login() {
-      window.alert("ログインしろよ");
+    // login() {
+    //   window.alert("ログインしろよ");
+    // }
+    //ボタン押下でv-model="text"で取得した値をconsol.logで表示する関数
+    // checkTextValue() {
+    //   console.log(this.text);
+    // }
+    addMessage() {
+      //日本語の入力時、変換の際、押されるenterキーの keyCode が 229 なので、その時はreturnしてメッセージを保存しないようにする
+      // if (event.keyCode === 229) {
+      //   return;
+      // }
+
+      //keyCode が 229という数字は可読性がよくないので、わかりやすくするためメソッドにkeyDownedForJPConversionを追加してそこで日本語変換の際のenterキーが押されたか判別する
+      if (this.keyDownedForJPConversion(event)) {
+        return;
+      }
+      const channelId = this.$route.params.id;
+      db.collection("channels")
+        .doc(channelId)
+        .collection("messages")
+        .add({ text: this.text })
+        .then(() => {
+          //メッセージ送信後にテキストエリアを空にしたいのでnullをtextに代入
+          this.text = null;
+          //alert("メッセージの保存に成功しました");
+        });
+    },
+    //日本語変換のためにenterキーが押されたが判別するメソッド
+    keyDownedForJPConversion(event) {
+      //日本語変換のためにenterキーのキーコードは229なのでcodeForConversionに代入
+      //キーコードを代入することによって意味をわかりやすいものにする
+      //※229みたいなコードを書いた本人しかわからない番号はマジックナンバーと言って好ましくないので変数もしくは定数にしておく
+      const codeForConversion = 229;
+      return event.keyCode === codeForConversion;
     }
+    //第一引数のeventは実行されたイベントの内容を取得する引数
+    // addMessage(event) {
+    //キーコード取得用のデバッガー
+    //   debugger;
+    // }
   }
 };
 </script>
