57b5f176201436724fb9f06ba228d0380ca5c5dd
Tatsuo Sato <tatsuosato0908@gmail.com>
2020-01-19 04:07:54 +0900

    表示用ダミーのメッセージコンポーネントの削除、親コンポーネントからメッセージ本文を受け取りそれをv-forで表示

--- components/Messages.vue
+++ components/Messages.vue
@@ -1,32 +1,20 @@
 <template>
   <div class="chats-container">
-    <message />
-    <message />
-    <message />
-    <message />
-    <message />
-    <message />
-    <message />
-    <message />
-    <message />
-    <message />
-    <message />
-    <message />
-    <message />
-    <message />
-    <message />
-    <message />
-    <message />
-    <message />
-    <message />
+    <message v-for="message in messages" :key="message.id" :message="message" />
   </div>
 </template>
 
 <script>
 import Message from "~/components/Message.vue";
 export default {
+  //親コンポーネント_id.vueからmessages: []の値を受け取るにはprops(※propertiesの略)を使う
+  //props: ["messages"]は親コンポーネントのtemplate内のバインドした値(messages)と同じものにする
+  props: ["messages"],
   components: {
     Message
+  },
+  mounted() {
+    console.log(this.messages);
   }
 };
 </script>
