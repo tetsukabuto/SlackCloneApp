7817fbdda41d099c4a78878a1113406848637b7d
Tatsuo Sato <tatsuosato0908@gmail.com>
2020-01-19 04:10:20 +0900

    メッセージリストのuserの情報(名前、アイコン画像)がまだ設定+取得できてないので一旦コメントアウト

--- components/Message.vue
+++ components/Message.vue
@@ -3,13 +3,13 @@
     <div class="thumbnail-container">
       <!-- HTML タグの属性(attributes)でdataの値を扱うにはv-bindを使う必要あり。src以外だとclass、id、style、hrefなど -->
       <!-- v-bindはセミコロンで書き方を省略できる。例えば今回の場合でいうと:srcのように -->
-      <img v-bind:src="message.user.thumbnail" />
+      <!-- <img v-bind:src="message.user.thumbnail" /> -->
     </div>
     <div class="message-container">
       <!-- dataメソッドのmessageの値をmustache構文で表示 -->
       <!-- <div class="user-name">{{ message.user.name }}</div> -->
       <!-- computedで定義された値をHTMLで表示させるには定義したメソッド名をmustache構文内に記述する この場合は{{ diplayName }}-->
-      <div class="user-name">{{ diplayName }}</div>
+      <!-- <div class="user-name">{{ diplayName }}</div> -->
       <div class="message">{{ message.text }}</div>
     </div>
   </div>
@@ -18,19 +18,22 @@
 <script>
 export default {
   //特定の値を保持するためdataメソッドを定義
-  data() {
-    //returnで返されたオブジェクトの値をコンポーネントで自由自在に使うことができる
-    return {
-      message: {
-        text: "今日も良い天気ですね",
-        user: {
-          thumbnail:
-            "https://pbs.twimg.com/profile_images/1131882065337602049/0JRUGd3S_400x400.jpg",
-          name: "note"
-        }
-      }
-    };
-  },
+  //   data() {
+  //     //returnで返されたオブジェクトの値をコンポーネントで自由自在に使うことができる
+  //     return {
+  //       message: {
+  //         text: "今日も良い天気ですね",
+  //         user: {
+  //           thumbnail:
+  //             "https://pbs.twimg.com/profile_images/1131882065337602049/0JRUGd3S_400x400.jpg",
+  //           name: "note"
+  //         }
+  //       }
+  //     };
+  //   },
+
+  //親コンポーネントからチャットメッセージを受け取る
+  props: ["message"],
   computed: {
     //diplayNameはdataのmessage.user.nameの値を取得して@をつけてreturnする
     diplayName() {
