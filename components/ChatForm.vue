9c2d954a7b86e94871de4b245a1a053d7217b112
Tatsuo Sato <tatsuosato0908@gmail.com>
2020-01-27 02:50:47 +0900

    Googleアカウントでログイン機能をindex.vueからChatform.vueに移植とログインボタン画像追加

--- components/ChatForm.vue
+++ components/ChatForm.vue
@@ -9,14 +9,33 @@
 
     <!-- v-modelはHTMLの値が変わるとJavaScriptのデータの値が変わる仕組みのディレクティブ(双方向データバインディング) -->
     <!-- v-on:keydown.enterはエンターが押されたらaddMessageを実行 -->
-    <textarea v-model="text" v-on:keydown.enter="addMessage"></textarea>
+    <textarea
+      v-model="text"
+      v-on:click="openLoginModal"
+      v-on:keydown.enter="addMessage"
+    ></textarea>
 
     <!-- ボタン押下でv-model="text"で取得した値をconsol.logで表示 -->
     <!-- <button v-on:click="checkTextValue">値を確認!</button> -->
+
+    <!-- ElDialogはElementUIのモーダルを表示するコンポーネント -->
+    <!-- titleはモーダルに表示されるタイトル -->
+    <!-- :visibleはdialogVisibleの値(boolean値)をバインディングしモーダルの表示/非表示の設定を受け取る -->
+    <el-dialog title="" :visible.sync="dialogVisible" width="30%">
+      <div class="image-container">
+        <img src="~/assets/google_sign_in.png" v-on:click="login" />
+      </div>
+    </el-dialog>
   </div>
 </template>
 <script>
-import { db } from "~/plugins/firebase";
+import { db, firebase } from "~/plugins/firebase";
+
+//ElementUIのモーダルを読み込む設定
+import Vue from "vue";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+Vue.use(ElementUI);
 
 export default {
   data() {
@@ -24,11 +43,17 @@ export default {
       // v-model="text"で取得した値の初期値
       //text: "テスト"
 
+      //モーダルは初期設定は非表示なのでfalseを設定
+      dialogVisible: false,
       text: null
     };
   },
   //methodsはmethods内に独自のfunctionを定義してそれを呼び出す
   methods: {
+    openLoginModal() {
+      this.dialogVisible = true;
+    },
+
     //フォーム欄をクリックしたときポップアップのアラートを呼び出すメソッド
     // login() {
     //   window.alert("ログインしろよ");
@@ -72,12 +97,33 @@ export default {
       //※229みたいなコードを書いた本人しかわからない番号はマジックナンバーと言って好ましくないので変数もしくは定数にしておく
       const codeForConversion = 229;
       return event.keyCode === codeForConversion;
-    }
+    },
     //第一引数のeventは実行されたイベントの内容を取得する引数
     // addMessage(event) {
     //キーコード取得用のデバッガー
     //   debugger;
     // }
+    login() {
+      //Providerを定義する:firebaseにはいろいろなログイン方法があるのでどのログイン方法を使うのか指定する(今回はGooleアカウントを使用)
+      const provider = new firebase.auth.GoogleAuthProvider();
+      //firebase.auth()のsinginメソッドを実行:Providerを引数にしてsinginメソッドを実行
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        //thenでログインに成功した時の処理を書いている
+        .then(result => {
+          //ユーザー情報を取得して
+          const user = result.user;
+          //console.logで出力
+          console.log(user);
+          //ログイン成功したらモーダル閉じる
+          this.dialogVisible = false;
+        })
+        //catchで失敗した時の処理を書く
+        .catch(error => {
+          window.alert(error);
+        });
+    }
   }
 };
 </script>
@@ -90,5 +136,15 @@ export default {
 textarea {
   width: 100%;
   height: 100%;
+}
+
+.image-container {
+  display: flex;
+  justify-content: center;
+}
+
+img {
+  width: 70%;
+  cursor: pointer;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,10 @@
 <template>
-  <div class="container">
-    <button v-on:click="login">ログイン</button>
-  </div>
+  <div class="container"></div>
 </template>
 
 <script>
 import { firebase } from "~/plugins/firebase";
 export default {
-  methods: {
-    login() {
-      window.alert("ログインするよ");
-
-      //Providerを定義する:firebaseにはいろいろなログイン方法があるのでどのログイン方法を使うのか指定する(今回はGooleアカウントを使用)
-      const provider = new firebase.auth.GoogleAuthProvider();
-      //firebase.auth()のsinginメソッドを実行:Providerを引数にしてsinginメソッドを実行
-      firebase
-        .auth()
-        .signInWithPopup(provider)
-        //thenでログインに成功した時の処理を書いている
-        .then(result => {
-          //ユーザー情報を取得して
-          const user = result.user;
-          //console.logで出力
-          console.log(user);
-        })
-        //catchで失敗した時の処理を書く
-        .catch(error => {
-          window.alert(error);
-        });
-    }
-  }
+  methods: {}
 };
 </script>
