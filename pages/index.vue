b425346a1bc4fa067f2160985033f2e9125bfff0
Tatsuo Sato <tatsuosato0908@gmail.com>
2020-01-27 02:07:44 +0900

    Googleアカウントでログイン機能実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,34 @@
 <template>
-  <div class="container"></div>
+  <div class="container">
+    <button v-on:click="login">ログイン</button>
+  </div>
 </template>
+
+<script>
+import { firebase } from "~/plugins/firebase";
+export default {
+  methods: {
+    login() {
+      window.alert("ログインするよ");
+
+      //Providerを定義する:firebaseにはいろいろなログイン方法があるのでどのログイン方法を使うのか指定する(今回はGooleアカウントを使用)
+      const provider = new firebase.auth.GoogleAuthProvider();
+      //firebase.auth()のsinginメソッドを実行:Providerを引数にしてsinginメソッドを実行
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        //thenでログインに成功した時の処理を書いている
+        .then(result => {
+          //ユーザー情報を取得して
+          const user = result.user;
+          //console.logで出力
+          console.log(user);
+        })
+        //catchで失敗した時の処理を書く
+        .catch(error => {
+          window.alert(error);
+        });
+    }
+  }
+};
+</script>
