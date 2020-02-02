36c4fce4f0d7192fba9d171993a39fb345b0819e
Tatsuo Sato <tatsuosato0908@gmail.com>
2020-02-03 03:11:33 +0900

    vuexの情報を扱うためにthis.$storeを使い、stateはvuexでデータを管理している場所をlogで参照

--- pages/index.vue
+++ pages/index.vue
@@ -5,6 +5,13 @@
 <script>
 import { firebase } from "~/plugins/firebase";
 export default {
+  mounted() {
+    //vuexの情報を扱うためには基本的にthis.$storeを使う
+    // stateはvuexでデータを管理している場所。Vueでいうところのdata
+    //   console.log(this.$store.state.user);
+
+    console.log(this.$store.getters.isAuthenticated);
+  },
   methods: {}
 };
 </script>
